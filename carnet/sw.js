0d29075c000b098ace811362335f5c9ce0188153
transmarrakech-create <transmarrakech@gmail.com>
2026-07-23 11:06:36 +0100

    Publication carnet (PWA chiffre) sous /carnet/

---
+++ carnet/sw.js
@@ -0,0 +1,23 @@
+/* Service worker du carnet - genere, ne pas editer a la main */
+const CACHE = 'carnet-0a06540c';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon-180.png'];
+self.addEventListener('install', function (e) {
+  e.waitUntil(caches.open(CACHE).then(function (c) { return c.addAll(ASSETS); }).then(function () { return self.skipWaiting(); }));
+});
+self.addEventListener('activate', function (e) {
+  e.waitUntil(caches.keys().then(function (ks) {
+    return Promise.all(ks.filter(function (k) { return k !== CACHE; }).map(function (k) { return caches.delete(k); }));
+  }).then(function () { return self.clients.claim(); }));
+});
+self.addEventListener('fetch', function (e) {
+  var req = e.request;
+  if (req.method !== 'GET') return;
+  if (req.mode === 'navigate') {
+    e.respondWith(fetch(req).then(function (resp) {
+      var copy = resp.clone(); caches.open(CACHE).then(function (c) { c.put('./index.html', copy); });
+      return resp;
+    }).catch(function () { return caches.match('./index.html'); }));
+    return;
+  }
+  e.respondWith(caches.match(req).then(function (r) { return r || fetch(req); }));
+});
