c785f6bf0ff7c8d640677908655d6d8861fbe9e0
transmarrakech-create <transmarrakech@gmail.com>
2026-08-30 19:07:36 +0100

    Publication carnet (PWA chiffre) sous /carnet/

--- carnet/sw.js
+++ carnet/sw.js
@@ -1,5 +1,5 @@
 /* Service worker du carnet - genere, ne pas editer a la main */
-const CACHE = 'carnet-0a06540c';
+const CACHE = 'carnet-d936b246';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon-180.png'];
 self.addEventListener('install', function (e) {
   e.waitUntil(caches.open(CACHE).then(function (c) { return c.addAll(ASSETS); }).then(function () { return self.skipWaiting(); }));
